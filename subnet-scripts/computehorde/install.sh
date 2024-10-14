34405c534521808cef1bc9263cc90410beb3f05f
drunest <venus1004@proton.me>
2024-10-14 16:19:31 -0500

    fix: add double quote for env variable

--- subnet-scripts/computehorde/install.sh
+++ subnet-scripts/computehorde/install.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 #no GPU
 
-if [ -f .env ]; then
-    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
-fi
+export $(awk '!/^#/ && /=/ {print}' .env)
 
-if [ -d ~/.bittensor/subnets/computehorde/ ]; then
-    cd ~/.bittensor/subnets/computehorde/
-else
-    echo "Directory ~/.bittensor/subnets/computehorde/ does not exist. Exiting."
-    exit 1
-fi
+cd ~/.bittensor/subnets/computehorde/
 sudo docker compose down
 
 sudo docker compose up -d
